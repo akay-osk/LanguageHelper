c53ba7ac9a910580da731228be06e6bbf906ebad
akane <akane@localhost>
2025-04-13 23:29:50 +0900

    2nd commit

--- src/main/java/com/example/demo/LanguageLearningHelperApplication.java
+++ src/main/java/com/example/demo/LanguageLearningHelperApplication.java
@@ -1,12 +1,7 @@
 package com.example.demo;
 
-import java.time.LocalDateTime;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.demo.entity.Vocab;
-import com.example.demo.service.VocabService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,35 +10,35 @@ import lombok.RequiredArgsConstructor;
 public class LanguageLearningHelperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LanguageLearningHelperApplication.class, args)
-		.getBean(LanguageLearningHelperApplication.class).exe();
-	}
-	
-	//DI
-	private final VocabService service;
-	
-	public void exe() {
-		//Search All Items
-		System.out.println("=== SEARCH ALL ITEMS ===");
-		for(Vocab row : service.findAllVocabs()) {
-			System.out.println(row);
-		}
-		//Search One Item
-		System.out.println("=== SEARCH ONE ITEM ===");
-		System.out.println(service.findByIdVocab(1));
-		
-		//Resister
-		//Create a data
-		Vocab word= new Vocab();
-		word.setWord("Service Test: étoile");
-		word.setPronunciation("Service Test: etwal");
-		word.setPartOfSpeech("Service Test: noun (feminine)");
-		word.setTranslation("Service Test: star");
-		word.setCreatedAt(LocalDateTime.now());
-		service.insertVocab(word);
-		System.out.println("=== Resistered Successfully ===");
-		System.out.println(service.findByIdVocab(2));
-		
+		SpringApplication.run(LanguageLearningHelperApplication.class, args);
+//		.getBean(LanguageLearningHelperApplication.class).exe();
+//	}
+//	
+//	//DI
+//	private final VocabService service;
+//	
+//	public void exe() {
+//		//Search All Items
+//		System.out.println("=== SEARCH ALL ITEMS ===");
+//		for(Vocab row : service.findAllVocabs()) {
+//			System.out.println(row);
+//		}
+//		//Search One Item
+//		System.out.println("=== SEARCH ONE ITEM ===");
+//		System.out.println(service.findByIdVocab(1));
+//		
+//		//Resister
+//		//Create a data
+//		Vocab word= new Vocab();
+//		word.setWord("Service Test: étoile");
+//		word.setPronunciation("Service Test: etwal");
+//		word.setPartOfSpeech("Service Test: noun (feminine)");
+//		word.setTranslation("Service Test: star");
+//		word.setCreatedAt(LocalDateTime.now());
+//		service.insertVocab(word);
+//		System.out.println("=== Resistered Successfully ===");
+//		System.out.println(service.findByIdVocab(2));
+//		
 //		//Update
 //		Vocab target= service.findByIdVocab(1);
 //		target.setPartOfSpeech("Service Test: phr");
